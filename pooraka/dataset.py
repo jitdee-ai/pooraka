5facf8bceff9887d87c4f104acf3061378e6c803
Chakkrit Termritthikun <kongpasom@gmail.com>
2020-09-16 00:12:14 +0930

    add autoaugment

--- pooraka/dataset.py
+++ pooraka/dataset.py
@@ -3,6 +3,30 @@ import torch.nn as nn
 import torch.utils
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
+import numpy as np
+from pooraka.autoaugment import CIFAR10Policy
+
+class Cutout(object):
+    def __init__(self, length):
+        self.length = length
+
+    def __call__(self, img):
+        h, w = img.size(1), img.size(2)
+        mask = np.ones((h, w), np.float32)
+        y = np.random.randint(h)
+        x = np.random.randint(w)
+
+        y1 = np.clip(y - self.length // 2, 0, h)
+        y2 = np.clip(y + self.length // 2, 0, h)
+        x1 = np.clip(x - self.length // 2, 0, w)
+        x2 = np.clip(x + self.length // 2, 0, w)
+
+        mask[y1: y2, x1: x2] = 0.
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img *= mask
+        return img
+
 
 def _data_transforms_cifar10(args):
   CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
--- pooraka/fit.py
+++ pooraka/fit.py
@@ -3,20 +3,23 @@ from pooraka.trainer import train_cifar, train_imagenet
 from pooraka.inference import infer_cifar, infer_imagenet
 from pooraka.dataset import get_cifar_queue
 from pooraka.utils import save_checkpoint
+import torch.nn as nn
 
 class learning:
-    def __init__(self, args, model):
+    def __init__(self, args, model, save_model, logging):
         self.args = args
         self.model = model
+        self.save_model = save_model
+        self.logging = logging
 
         self.optimizer = torch.optim.SGD(self.model.parameters(), 
                                     self.args.learning_rate, 
                                     momentum=self.args.momentum, 
                                     weight_decay=self.args.weight_decay)
 
-        self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, 
-                                                                    float(self.args.epochs), 
-                                                         eta_min=0.0)
+        self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer,
+                                                                    float(self.args.epochs),
+                                                                    eta_min=0.0)
         self.criterion = nn.CrossEntropyLoss().cuda()
 
         self.train_queue = None
@@ -30,7 +33,7 @@ class learning:
 
     def run(self):
 
-        self.load_data(self)
+        self.load_data()
         
         for epoch in range(self.args.epochs):
             if self.args.dataset.startswith('cifar'):
@@ -39,25 +42,27 @@ class learning:
                                                     self.model, 
                                                     self.criterion, 
                                                     self.optimizer, 
-                                                    logging_mode=True)
+                                                    logging_mode=self.logging)
                 valid_acc, valid_obj = inference.infer_cifar(self.args, 
                                                     self.valid_queue, 
                                                     self.model, 
                                                     self.criterion, 
-                                                    logging_mode=True)
+                                                    logging_mode=self.logging)
 
                 self.scheduler.step()
 
                 is_best = valid_acc > self.best_acc1
                 self.best_acc1 = max(valid_acc, self.best_acc1)
 
-                save_checkpoint({
-                    'epoch': epoch + 1,
-                    'arch': self.args.arch,
-                    'state_dict': self.model.state_dict(),
-                    'best_acc1': self.best_acc1,
-                    'optimizer' : self.optimizer.state_dict(),
-                }, is_best, self.args.save)
-            
-            logging.info('best_acc1 %f', best_acc1)
+                if self.save_model:
+                    save_checkpoint({
+                        'epoch': epoch + 1,
+                        'arch': self.args.arch,
+                        'state_dict': self.model.state_dict(),
+                        'best_acc1': self.best_acc1,
+                        'optimizer' : self.optimizer.state_dict(),
+                    }, is_best, self.args.save)
+            if self.logging:
+                logging.info('best_acc1 %f', best_acc1)
+
 
