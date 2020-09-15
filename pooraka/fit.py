113a9c0b91fa3a915f6253de8e6eee0313b718dc
Chakkrit Termritthikun <kongpasom@gmail.com>
2020-09-15 23:46:02 +0930

    update fit

--- pooraka/fit.py
+++ pooraka/fit.py
@@ -2,6 +2,7 @@ import torch
 from pooraka.trainer import train_cifar, train_imagenet
 from pooraka.inference import infer_cifar, infer_imagenet
 from pooraka.dataset import get_cifar_queue
+from pooraka.utils import save_checkpoint
 
 class learning:
     def __init__(self, args, model):
@@ -15,11 +16,13 @@ class learning:
 
         self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, 
                                                                     float(self.args.epochs), 
-                                                                    eta_min=0.0)
+                                                         eta_min=0.0)
         self.criterion = nn.CrossEntropyLoss().cuda()
 
         self.train_queue = None
         self.valid_queue = None
+
+        self.best_acc1 = 0
 
     def load_data(self):
         if self.args.dataset.startswith('cifar'):
@@ -45,4 +48,16 @@ class learning:
 
                 self.scheduler.step()
 
+                is_best = valid_acc > self.best_acc1
+                self.best_acc1 = max(valid_acc, self.best_acc1)
+
+                save_checkpoint({
+                    'epoch': epoch + 1,
+                    'arch': self.args.arch,
+                    'state_dict': self.model.state_dict(),
+                    'best_acc1': self.best_acc1,
+                    'optimizer' : self.optimizer.state_dict(),
+                }, is_best, self.args.save)
+            
+            logging.info('best_acc1 %f', best_acc1)
 
