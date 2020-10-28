91d95d95167abab2f679e5d232108426f92cc792
Chakkrit Termritthikun <kongpasom@gmail.com>
2020-10-28 18:11:47 +1030

    Remove fit function
    Remove fit function

--- pooraka/__init__.py
+++ pooraka/__init__.py
@@ -1,7 +1,6 @@
 from .version import __version__
 from .flops_counter import *
 from .meanstd import *
-from .fit import learning
 from .utils import CrossEntropyLabelSmooth , get_learning_rate, create_exp_dir, save_checkpoint
 from .autoaugment import *
 from .mix import *
