f9eb7e8d36d2ce245448c9e8f16d258dc958404c
Chakkrit Termritthikun <kongpasom@gmail.com>
2020-10-28 18:09:31 +1030

    Fixed error
    Fixed error

--- pooraka/__init__.py
+++ pooraka/__init__.py
@@ -5,3 +5,4 @@ from .fit import learning
 from .utils import CrossEntropyLabelSmooth , get_learning_rate, create_exp_dir, save_checkpoint
 from .autoaugment import *
 from .mix import *
+from .dataset import *
